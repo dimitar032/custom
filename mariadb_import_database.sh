058ff1d2825e97921b8f4cd0665144b49ce11ee0
Dimitar Dimitrov <dimitar032@gmail.com>
2018-02-16 21:49:12 +0200

    Update mariadb_import_database.sh

--- mariadb_import_database.sh
+++ mariadb_import_database.sh
@@ -9,24 +9,22 @@ read i_database_name
 echo -n "This operation will DROP database "\""${i_database_name}"\"" are you sure? say "\""yes"\"": " 
 read i_agreement 
 
-if [ "$i_agreement" != "yes" ]
+if [ "$i_agreement" == "yes" ]
 then
-	exit
+	echo "Dropping database..."
+	mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} <<EOF
+	DROP DATABASE IF EXISTS ${i_database_name};
+	EOF
+	echo "Dropped database."
+
+	mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} <<EOF
+	CREATE DATABASE ${i_database_name}
+	CHARACTER SET "utf8"
+	COLLATE "utf8_general_ci"
+	EOF
+	echo "Created database."
+
+	echo "Begin importing..."
+	mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} ${i_database_name} < ${V_FILE_DIRECTORY}${i_database_name}.sql
+	echo "Done."
 fi
-
-echo "Dropping database..."
-mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} <<EOF
-DROP DATABASE IF EXISTS ${i_database_name};
-EOF
-echo "Dropped database."
-
-mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} <<EOF
-CREATE DATABASE ${i_database_name}
-CHARACTER SET "utf8"
-COLLATE "utf8_general_ci"
-EOF
-echo "Created database."
-
-echo "Begin importing..."
-mysql -u ${V_DATABASE_USER_NAME} -p${V_DATABASE_USER_PASSWORD} ${i_database_name} < ${V_FILE_DIRECTORY}${i_database_name}.sql
-echo "Done."
