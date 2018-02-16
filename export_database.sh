e97eb4f7cbf306536fd8df08ac537e0ee04a1358
Dimitar Dimitrov <dimitar032@gmail.com>
2018-02-16 19:00:24 +0200

    export databse with options

---
+++ export_database.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+V_DATABASE_NAME=DATABASE;
+V_DATABASE_USER_NAME=admin;
+V_TIMESTAMP=$(date +%Y_%m_%d)
+V_FILENAME="${V_TIMESTAMP}-${V_DATABASE_NAME}.sql"
+
+echo -n "Enter a password for db.user "\""${V_DATABASE_USER_NAME}"\"": "
+read -s i_password
+echo 
+
+echo -n "Do you want to ignore specific tables data? say "\""yes"\"": " 
+read i_agreement
+
+if [ "$i_agreement" = "yes" ]
+then
+	echo "Exporting "\""${V_DATABASE_NAME}"\"" with specific tables data..."
+	mysqldump -u ${V_DATABASE_USER_NAME} -p${i_password} ${V_DATABASE_NAME} --routines > ${V_FILENAME}
+else
+	echo "Exporting "\""${V_DATABASE_NAME}"\"" without specific tables data..."
+
+	#add tables structure + data
+	mysqldump -u ${V_DATABASE_USER_NAME} -p${i_password} ${V_DATABASE_NAME} \
+	--routines \
+	--ignore-table=${V_DATABASE_NAME}.specific_table_1 \
+	--ignore-table=${V_DATABASE_NAME}.specific_table_2 \
+	> ${V_FILENAME}	&& \
+
+	#get only the structure of ignored tables..
+	mysqldump -u ${V_DATABASE_USER_NAME} -p${i_password} ${V_DATABASE_NAME} \
+	 specific_table_1 \
+	 specific_table_2 \
+	--no-data >> ${V_FILENAME}
+fi
+echo "Done."
